fd1a48d0ffb7a628834225db6ff74f2927defaaf
Jesse Rosenberger <git@jro.cc>
2019-05-30 14:52:42 +0300

    Remove trailing comma for Node.js 6.
    Plus re-factor to avoid re-Prettier back to broken state.

--- packages/apollo-gateway/jest.config.js
+++ packages/apollo-gateway/jest.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const config = require('../../jest.config.base');
 
-module.exports = Object.assign(
-  {
-    setupFilesAfterEnv: [
-      path.resolve(__dirname, './src/__tests__/testSetup.ts'),
-    ],
-  },
-  config,
-);
+const additionalConfig = {
+  setupFilesAfterEnv: [path.resolve(__dirname, './src/__tests__/testSetup.ts')],
+};
+module.exports = Object.assign(Object.create(null), additionalConfig, config);
